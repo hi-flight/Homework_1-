85b4393307fb3407fa98d367d372a29516ed5e17
unlimitedglassworks <66311794+TubofGlasses@users.noreply.github.com>
2024-02-01 14:57:29 +0800

    Stuff

---
+++ game.cpp
@@ -0,0 +1,55 @@
+#include <raylib.h>
+
+float screenwidth = 800;
+float screenheight = 600;
+Vector2 targetDestination;
+
+Rectangle box = Rectangle{screenwidth/2 - 150, screenheight/2 - 150, 300, 300};
+
+int main() {
+    InitWindow(screenwidth, screenheight, "Lim, Sta. Cruz, Tadiarca_Homework01");
+    SetTargetFPS(60);
+
+    int cam_type;
+    Camera2D camera_view = {0};
+    float EDGE_X[2], EDGE_Y[2];
+    // camera's focus
+    camera_view.target = { screenwidth/2, screenheight/2 };
+    //camera's offset
+    camera_view.offset = { screenwidth/2, screenheight/2 };
+    // camera’s zoom
+    camera_view.zoom = 1.0f;
+
+    while (!WindowShouldClose()) {
+        float delta_time = GetFrameTime();
+        targetDestination = GetMousePosition();
+
+        if (targetDestination.y < box.y) {
+            box.y -= 300 * delta_time;
+        }
+        if (targetDestination.x < box.x) {
+            box.x -= 300 * delta_time;
+        } 
+        if (targetDestination.y > box.x) {
+            box.x += 300 * delta_time;
+        } 
+        if (targetDestination.x > box.x) {
+            box.x += 300 * delta_time;
+        } 
+
+        camera_view.target = Vector2{ box.x + box.width / 2, box.y + box.height / 2 };
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        BeginMode2D(camera_view);
+        DrawRectangleLines(box.x, box.y, box.width, box.height, WHITE);
+        DrawRectangle(screenwidth/2, screenheight/2, 400, 400, GREEN);
+        EndMode2D();
+
+        EndDrawing();
+    }
+    CloseWindow();
+
+    return 0;
+}
